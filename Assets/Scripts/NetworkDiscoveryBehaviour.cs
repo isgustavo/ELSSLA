3c5af9e33714263f96a368e99f25ddbd1a6959e9
Gustavo F Oliveira <hi.gustavo.oliveira@gmail.com>
2017-01-27 12:14:22 -0200

    Add custom objects to Network manager and Network discovery

---
+++ Assets/Scripts/NetworkDiscoveryBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkDiscoveryBehaviour : NetworkDiscovery {
+
+	public class ServerGame {
+
+		private string _serverName;
+		public string ServerName {
+			get { return _serverName; }
+		}
+
+		private string _serverIp;
+		public string ServerIp {
+			get { return _serverIp; }
+		}
+
+		public ServerGame (string name, string ip) {
+			_serverName = name;
+			_serverIp = ip;
+		}
+	}
+
+	private ServerGame _server;
+	public ServerGame Server {
+		get { return _server; }
+	}
+
+	void Start () {
+		Initialize();
+		StartAsClient();
+	}
+
+	public override void OnReceivedBroadcast (string fromAddress, string data) {
+
+		//Network discovery component has found a server being broadcasted
+		ServerFound (fromAddress, data);
+	}
+
+	private void ServerFound (string ip, string data) {
+
+		if (_server == null) {
+			_server = new ServerGame (data, ip);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/NetworkManagerBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerBehaviour : NetworkManager {
+
+	private const int NETWORK_PORT = 7777;
+
+	[SerializeField]
+	private NetworkDiscoveryBehaviour discovery;
+
+
+	/// <summary>
+	/// Taps to play.
+	/// </summary>
+	public void TapToPlay () {
+
+		networkAddress = Network.player.ipAddress;
+		networkPort = NETWORK_PORT;
+
+		StartHost ();
+	}
+
+	/// <summary>
+	/// Raises the start host event. Called after StartHost ()
+	/// </summary>
+	public override void OnStartHost () {
+		//To stop current search for a server
+		discovery.StopBroadcast ();
+
+		//To start game being server
+		discovery.broadcastData = networkPort.ToString ();
+		discovery.StartAsServer ();
+
+	}
+
+	public void TapToJoin() {
+
+		networkAddress = discovery.Server.ServerIp;
+		StartClient();
+	}
+		
+}
